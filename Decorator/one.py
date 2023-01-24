2a0cb60f17fa02d37a68ffb5802077aeff243fd8
Joshnagowd <joshigowdramagowni721@gmail.com>
2023-01-24 18:35:01 +0530

    python

---
+++ Decorator/one.py
@@ -0,0 +1,13 @@
+def addPosition(func):
+    def greet(name):
+        if name :"modi"
+        return 'PM'
+        else:
+        return greet(name)
+return greet
+@addPosition
+def wish(name):
+    return "Hello"+name
+wish ("rahul")
+wish("modi")
+wish("ram")
---
+++ Jumping statements/one.py
@@ -0,0 +1,9 @@
+#Print 0 , 2, 4,6 ,8 ,10 ,12 ,14 , 16 , 18 , 20
+'''for x in range(0 , 21 , 2):
+    print(x)'''
+for x in range(21):
+    if x%2 !=0:
+        
+        continue
+    print(x)
+    
